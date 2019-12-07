a765e192e286b6f036d9be7d97869ed7185044dc
Evgeniy Belyaev <>
2019-12-07 16:56:42 +0300

    + 2 quizes

--- app/screens/drawer.js
+++ app/screens/drawer.js
@@ -50,8 +50,8 @@ class DrawerComponent extends Component<Props, {}> {
                 <Item open={openQuiz} title='Насколько вы образованы?' uri='https://everbly.com/ru/%D1%8D%D1%80%D1%83%D0%B4%D0%B8%D1%82'></Item>
                 <Item open={openQuiz} title='Хорошо ли вы знаете Россию?' uri='https://everbly.com/ru/%D1%85%D0%BE%D1%80%D0%BE%D1%88%D0%BE-%D0%BB%D0%B8-%D0%B2%D1%8B-%D0%B7%D0%BD%D0%B0%D0%B5%D1%82%D0%B5-%D1%80%D0%BE%D1%81%D1%81%D0%B8%D1%8E'></Item>
                 <Item open={openQuiz} title='Что вы знаете о футболе?' uri='https://everbly.com/ru/soccer'></Item>
-                <Item open={openQuiz} title='Хорошо ли вы знаете Россию?' uri='https://everbly.com/ru/%D1%85%D0%BE%D1%80%D0%BE%D1%88%D0%BE-%D0%BB%D0%B8-%D0%B2%D1%8B-%D0%B7%D0%BD%D0%B0%D0%B5%D1%82%D0%B5-%D1%80%D0%BE%D1%81%D1%81%D0%B8%D1%8E'></Item>
-                <Item open={openQuiz} title='Что вы знаете о футболе?' uri='https://everbly.com/ru/soccer'></Item>
+                <Item open={openQuiz} title='Тест по географии' uri='https://everbly.com/ru/%D1%82%D0%B5%D1%81%D1%82-%D0%BF%D0%BE-%D0%B3%D0%B5%D0%BE%D0%B3%D1%80%D0%B0%D1%84%D0%B8%D0%B8?nq=nextquiz--%D1%85%D0%BE%D1%80%D0%BE%D1%88%D0%BE-%D0%BB%D0%B8-%D0%B2%D1%8B-%D0%B7%D0%BD%D0%B0%D0%B5%D1%82%D0%B5-%D1%80%D0%BE%D1%81%D1%81%D0%B8%D1%8E--%D1%82%D0%B5%D1%81%D1%82-%D0%BF%D0%BE-%D0%B3%D0%B5%D0%BE%D0%B3%D1%80%D0%B0%D1%84%D0%B8%D0%B8'></Item>
+                <Item open={openQuiz} title='Ух ты, коты!' uri='https://everbly.com/ru/%D1%83%D1%85-%D1%82%D1%8B-%D0%BA%D0%BE%D1%82%D1%8B?nq=nextquiz--%D1%85%D0%BE%D1%80%D0%BE%D1%88%D0%BE-%D0%BB%D0%B8-%D0%B2%D1%8B-%D0%B7%D0%BD%D0%B0%D0%B5%D1%82%D0%B5-%D1%80%D0%BE%D1%81%D1%81%D0%B8%D1%8E--%D1%83%D1%85-%D1%82%D1%8B-%D0%BA%D0%BE%D1%82%D1%8B'></Item>
             </SafeAreaView>
         );
     }
